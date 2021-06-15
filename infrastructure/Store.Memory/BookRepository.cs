1dda4fe90e77b7d4f13dfe5a7b76eb2ad348f65b
Solusigni <85879020+Solusigni@users.noreply.github.com>
2021-06-15 13:22:31 +0500

    Добавить страницу поиска

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -12,6 +12,7 @@ namespace Store.Memory
             new Book(2, "Refactoring"),
             new Book(3, "C Programming Language"),
         };
+
         public Book[] GetAllByTitle(string titlePart)
         {
             return books.Where(book => book.Title.Contains(titlePart))
